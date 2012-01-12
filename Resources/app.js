0981e69a8cbe5e57e953741ac9494c9e85d02565
Tony Lukasavage <anthony.lukasavage@gmail.com>
2012-01-12 15:13:26 -0500

    enforcing 1.8+ and not mobile web (for now)

--- Resources/app.js
+++ Resources/app.js
@@ -1,25 +1,33 @@
-// This is a single context application with multiple windows in a stack
-(function() {
-	//determine platform and form factor and render appropriate components
-	var osname = Ti.Platform.osname,
-		version = Ti.Platform.version,
-		height = Ti.Platform.displayCaps.platformHeight,
-		width = Ti.Platform.displayCaps.platformWidth;
-
-	//considering tablet to have one dimension over 900px - can define your own
-	var isTablet = osname === 'ipad' || (osname === 'android' && (width > 899 || height > 899));
-
-	var Window;
-	if (isTablet) {
-		Window = require('ui/tablet/ApplicationWindow').ApplicationWindow;
-	}
-	else {
-		if (osname === 'android') {
-			Window = require('ui/handheld/android/ApplicationWindow').ApplicationWindow;
+if (Ti.version < 1.8 ) {
+	alert('Sorry - this application template requires Titanium Mobile SDK 1.8 or later');
+}
+else if (Ti.Platform.osname === 'mobileweb') {
+	alert('Mobile web is not yet supported by this template');
+}
+else {
+	// This is a single context application with multiple windows in a stack
+	(function() {
+		//determine platform and form factor and render appropriate components
+		var osname = Ti.Platform.osname,
+			version = Ti.Platform.version,
+			height = Ti.Platform.displayCaps.platformHeight,
+			width = Ti.Platform.displayCaps.platformWidth;
+	
+		//considering tablet to have one dimension over 900px - can define your own
+		var isTablet = osname === 'ipad' || (osname === 'android' && (width > 899 || height > 899));
+	
+		var Window;
+		if (isTablet) {
+			Window = require('ui/tablet/ApplicationWindow').ApplicationWindow;
 		}
 		else {
-			Window = require('ui/handheld/ios/ApplicationWindow').ApplicationWindow;
+			if (osname === 'android') {
+				Window = require('ui/handheld/android/ApplicationWindow').ApplicationWindow;
+			}
+			else {
+				Window = require('ui/handheld/ios/ApplicationWindow').ApplicationWindow;
+			}
 		}
-	}
-	new Window().open();
-})();
+		new Window().open();
+	})();
+}
