11bb910b97f022f20fad564b9b3feade29248d99
tonylukasavage <anthony.lukasavage@gmail.com>
2012-03-07 17:16:02 -0500

    using local XML to get around cross origin security for mobile web, slightly different XML parsing

--- Resources/services/rss.js
+++ Resources/services/rss.js
@@ -1,8 +1,11 @@
-var RSS_URL = 'http://feeds.mashable.com/Mashable?format=xml';
+var osname = Ti.Platform.osname;
+var RSS_URL = osname === 'mobileweb' ? '/feed.xml' : 'http://feeds.mashable.com/Mashable?format=xml';
 var MONTH_MAP = { JAN: 1, FEB: 2, MAR: 3, APR: 4, MAY: 5, JUN: 6, JUL: 7, AUG: 8, SEP: 9, OCT: 10, NOV: 11, DEC: 12 };
 
 var getRssText = function(item, key) {
-	return item.getElementsByTagName(key).item(0).text;
+	return osname === 'mobileweb' ?
+			item.getElementsByTagName(key).item(0).childNodes[0].nodeValue :
+			item.getElementsByTagName(key).item(0).text;
 }
 
 var parseDate = function(dateString) {
@@ -14,7 +17,7 @@ var parseDate = function(dateString) {
 exports.loadRssFeed = function(o) {
 	var xhr = Titanium.Network.createHTTPClient();	
 	xhr.open('GET', RSS_URL);
-	xhr.onload = function() {
+	xhr.onload = function(e) {
 		var xml = this.responseXML;
 		var items = xml.documentElement.getElementsByTagName("item");
 		var data = [];
@@ -30,7 +33,7 @@ exports.loadRssFeed = function(o) {
 		}
 		if (o.success) { o.success(data); }
 	};
-	xhr.onerror = function() {
+	xhr.onerror = function(e) {
 		if (o.error) { o.error(); }
 	};
 
